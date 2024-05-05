0edc432a260f911b19d1aa2cebade70aecda6722
Joel Taylor <jtayped@gmail.com>
2024-05-05 23:01:51 +0200

    functions defined

--- elements/Post.py
+++ elements/Post.py
@@ -1,5 +1,21 @@
 from dataclasses import dataclass
-
+from elements.Comment import Comment
 
 @dataclass
-class Post: ...
+class Post:
+    id: str
+
+    def like(self) -> None:
+        pass
+
+    def get_likes(self) -> int:
+        pass
+
+    def get_images(self) -> list[str]:
+        pass
+
+    def get_comments(self, limit=10) -> list[Comment]:
+        pass
+
+    def comment(self) -> Comment:
+        pass
--- elements/User.py
+++ elements/User.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
 
 from utils.auth import check_auth
+from elements.Post import Post
+
 
 @dataclass
 class User:
@@ -10,11 +12,26 @@ class User:
     Args:
         name (str): Username of the user.
     """
-    name: str
-    
-    @check_auth
-    def follow(self):
-        print("authed")
 
-    def get_posts(self):
+    name: str
+
+    def is_private(self) -> bool:
+        pass
+
+    def follow(self) -> None:
+        pass
+
+    def unfollow(self) -> None:
+        pass
+
+    def get_followers(self) -> int:
+        pass
+
+    def get_following(self) -> int:
+        pass
+
+    def send_dm(self, message: str) -> None:
+        pass
+
+    def get_posts(self, limit=10) -> list[Post]:
         pass
