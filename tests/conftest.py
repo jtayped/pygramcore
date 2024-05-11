fda73e8ec3066cf04a5d3ed92522011783d287c2
Joel Taylor <jtayped@gmail.com>
2024-05-11 20:18:42 +0200

    random account changed

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,16 +2,19 @@ from pygramcore import User, Post
 import pytest
 
 DEFAULT_USER = "champagnepapi"
-PRIVATE_USER = "_gemmapel" # random acc.
+PRIVATE_USER = "2.modrego"  # random acc.
 DEFAULT_POST = "C5ZiDVruR2f"
+
 
 @pytest.fixture
 def user():
     return User(DEFAULT_USER)
 
+
 @pytest.fixture
 def private_user():
     return User(PRIVATE_USER)
+
 
 @pytest.fixture
 def post():
